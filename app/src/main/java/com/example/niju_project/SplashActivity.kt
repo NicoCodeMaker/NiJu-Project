bb13e308b0485d3a016cd7f38ccbd9831b43c5e8
Nicolas <nicolascastroolivero28@gmail.com>
2025-09-07 20:04:59 -0500

    Modificación splash 2

--- app/src/main/java/com/example/niju_project/SplashActivity.kt
+++ app/src/main/java/com/example/niju_project/SplashActivity.kt
@@ -15,6 +15,6 @@ class SplashActivity : AppCompatActivity() {
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish() // Cierra el splash para que no se pueda regresar
-        }, 3000)
+        }, 5000)
     }
 }
