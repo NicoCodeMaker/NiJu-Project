693a5d1135eae21e68e59115d10cef53be76314e
Nicolas <nicolascastroolivero28@gmail.com>
2025-09-07 19:45:29 -0500

    Agregación del Splash

---
+++ app/src/main/java/com/example/niju_project/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.niju_project
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Espera 3 segundos y abre MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish() // Cierra el splash para que no se pueda regresar
+        }, 3000)
+    }
+}
