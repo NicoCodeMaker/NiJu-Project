434ad3e10e3b4ebb7fe8998117c4ea50af452e0c
morajuli <morajulian610@gmail.com>
2025-09-21 16:46:20 -0500

    se crean los efectos de los linearlayouts, ademas de la integracion con el AndroidManifest.xml

--- app/src/main/java/com/example/niju_project/MainActivity.kt
+++ app/src/main/java/com/example/niju_project/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.niju_project
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) // tu layout con el texto "Bienvenido a NiJu English"
+
+        val btnComenzar = findViewById<Button>(R.id.startButton) // id del botón
+        btnComenzar.setOnClickListener {
+            // Abrir la otra pantalla
+            val intent = Intent(this, layoutMain::class.java)
+            startActivity(intent)
+            finish() // evita volver atrás al splash
+        }
     }
 }
